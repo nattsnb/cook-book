8c55095e0a229c40c76f27cba59800c5920e55b2
Natasza ASzatan <natiye.nb@gmai.com>
2025-02-10 20:40:58 +0100

    fixed unneded return and changed map into for each function

--- src/components/RecipeCard/Ingredients.tsx
+++ src/components/RecipeCard/Ingredients.tsx
@@ -4,6 +4,7 @@ import { Typography } from "@mui/material";
 import { StyledEntryContainer, StyledDivider } from "./RecipeCard.styled.tsx";
 import pluralize from "pluralize";
 import { ComponentContainer } from "../../shared/components/ComponentContainer.tsx";
+import { Ingredient } from "../../shared/types/Ingredient.ts";
 
 interface IngredientsProps {
   recipe: Recipe;
@@ -13,7 +14,7 @@ export const Ingredients = forwardRef<HTMLDivElement, IngredientsProps>(
   ({ recipe }, ingredientsRef) => {
     let ingredientsToDisplay: string[] = [];
 
-    recipe.ingredients.map((ingredient) => {
+    const pushPrintableVersion = (ingredient: Ingredient) => {
       if (ingredient.unit === "each") {
         ingredientsToDisplay.push(
           pluralize(ingredient.name, Number(ingredient.amount), true),
@@ -23,20 +24,21 @@ export const Ingredients = forwardRef<HTMLDivElement, IngredientsProps>(
           `${ingredient.amount} ${ingredient.unit} of ${ingredient.name}`,
         );
       }
-    });
+    };
+
+    recipe.ingredients.forEach(pushPrintableVersion);
+
     return (
       <ComponentContainer
         ref={ingredientsRef as React.RefObject<HTMLDivElement>}
       >
         <Typography variant="h3">Ingredients</Typography>
         <StyledDivider />
-        {ingredientsToDisplay.map((ingredient, index) => {
-          return (
-            <StyledEntryContainer key={index}>
-              {index + 1 + ". " + ingredient}
-            </StyledEntryContainer>
-          );
-        })}
+        {ingredientsToDisplay.map((ingredient, index) => (
+          <StyledEntryContainer key={index}>
+            {index + 1 + ". " + ingredient}
+          </StyledEntryContainer>
+        ))}
       </ComponentContainer>
     );
   },
